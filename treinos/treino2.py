fe22cffab55fc00bc01bfd9bf907b17ef1be067f
Pedro Pereira <62028225+pedrocpereira@users.noreply.github.com>
2021-03-25 18:20:48 +0000

    Update treino2.py

--- treinos/treino2.py
+++ treinos/treino2.py
@@ -205,3 +205,158 @@ def erdos(artigos,n):
     lista.sort(key = lambda y: numero[y])
                     
     return lista
+
+
+'''
+
+Implemente uma função que calcula um dos caminhos mais curtos para atravessar
+um labirinto. O mapa do labirinto é quadrado e representado por uma lista 
+de strings, onde um ' ' representa um espaço vazio e um '#' um obstáculo.
+O ponto de entrada é o canto superior esquerdo e o ponto de saída o canto
+inferior direito. A função deve devolver uma string com as instruções para
+atravesar o labirinto. As instruções podem ser 'N','S','E','O'.
+
+'''
+# 13%
+
+def caminho(mapa):
+    pai = {}
+    queue = [(0,0)]
+    fim = (len(mapa) - 1, len(mapa) - 1)
+    anterior = (0,0)
+    result = ''
+    
+    #travessia em largura
+    while queue:
+        x, y = queue.pop(0)
+        if (x, y) == fim:
+            break
+        for i in [-1, 1]:
+            if 0 <= x + i < len(mapa) and mapa[y][x + i] == ' ' and (x + i, y) not in pai:
+                pai[(x + i, y)] = (x, y)
+                queue.append((x + i, y))
+            if 0 <= y + i < len(mapa) and mapa[y + i][x] == ' ' and (x, y + i) not in pai:
+                pai[(x, y + i)] = (x, y)
+                queue.append((x, y + i))
+                
+    #caminho mais curto
+    caminho = [fim]
+    ponto = fim
+    while ponto != (0,0):
+        ponto = pai[ponto]
+        caminho.insert(0, ponto)
+        
+    while caminho:
+        atual = caminho.pop(0)
+        if atual[0] > anterior[0]:
+            result += 'E'
+        elif atual[0] < anterior[0]:
+            result += 'O'
+        elif atual[1] > anterior[1]:
+            result += 'S'
+        elif atual[1] < anterior[1]:
+            result += 'N'
+        anterior = atual
+    
+    return result
+
+
+'''
+
+Implemente uma função que calcula o menor custo de atravessar uma região de
+Norte para Sul. O mapa da região é rectangular, dado por uma lista de strings,
+onde cada digito representa a altura de cada ponto. Só é possível efectuar 
+movimentos na horizontal ou na vertical, e só é possível passar de um ponto
+para outro se a diferença de alturas for inferior ou igual a 2, sendo o custo 
+desse movimento 1 mais a diferença de alturas. O ponto de partida (na linha
+mais a Norte) e o ponto de chegada (na linha mais a Sul) não estão fixados à
+partida, devendo a função devolver a coordenada horizontal do melhor
+ponto para iniciar a travessia e o respectivo custo. No caso de haver dois pontos
+com igual custo, deve devolver a coordenada mais a Oeste.
+
+'''
+# 13%
+
+def dijkstra(mapa, o):
+    dist = {o: 0}
+    orla = {o}
+    while orla:
+        x, y = min(orla, key = lambda x: dist[x])
+        orla.remove((x, y))
+        for i in [-1, 1]:
+            dx = x + i
+            dy = y + i
+            if 0 <= dx < len(mapa[0]) and abs(int(mapa[y][dx]) - int(mapa[y][x])) <= 2 and (dx, y) not in dist:
+                orla.add((dx, y))
+                dist[(dx, y)] = 1 + abs(int(mapa[y][dx]) - int(mapa[y][x])) + dist[(x, y)]
+            if 1 <= dy < len(mapa) and abs(int(mapa[dy][x]) - int(mapa[y][x])) <= 2 and (x, dy) not in dist:
+                orla.add((x, dy))
+                dist[(x, dy)] = 1 + abs(int(mapa[dy][x]) - int(mapa[y][x])) + dist[(x, y)]
+    
+    result = [dist[x] for x in dist if x[1] == (len(mapa) - 1)]
+    return min(result) if result else float("inf")
+    
+def travessia(mapa):
+    dist = [(x, dijkstra(mapa, (x, 0))) for x in range(len(mapa[0]))]
+    dist.sort(key = lambda t: t[1])
+    return dist[0]
+
+
+'''
+
+Implemente uma função que calcula o preço mais barato para fazer uma viagem de
+autocarro entre duas cidades. A função recebe (para além das duas cidades) uma
+lista de rotas de autocarro, onde cada rota é uma sequência de cidades por onde
+passa o autocarro, intercalada com o custo para viajar entre cada par de cidades.
+Assuma que cada rota funciona nos dois sentidos.
+
+'''
+# 13%
+
+def build(rotas):
+    adj = {}
+    
+    for rota in rotas:
+        i = 0
+        while i + 2 < len(rota):
+            o = rota[i]
+            c = rota[i + 1]
+            d = rota[i + 2]
+            if o not in adj:
+                adj[o] = {}
+            if d not in adj:
+                adj[d] = {}
+            adj[o][d] = c
+            adj[d][o] = c
+            i += 2
+    
+    return adj
+    
+def fw(adj): 
+    dist = {} 
+    
+    for o in adj: 
+        dist[o] = {} 
+        for d in adj: 
+            if o == d: 
+                dist[o][d] = 0 
+            elif d in adj[o]: 
+                dist[o][d] = adj[o][d] 
+            else: 
+                dist[o][d] = float("inf")
+                
+    for k in adj: 
+        for o in adj: 
+            for d in adj: 
+                if dist[o][k] + dist[k][d] < dist[o][d]: 
+                    dist[o][d] = dist[o][k] + dist[k][d] 
+ 
+    return dist
+
+def viagem(rotas,o,d):
+    if o == d:
+        return 0
+    
+    dist = fw(build(rotas))
+    
+    return dist[o][d]
